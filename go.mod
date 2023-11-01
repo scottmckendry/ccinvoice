74536bf1df73626b5d9148fbbaa57cfd012bea0f
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-11-02 04:57:47 +1300

    Testing PDF generation

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.3
 
 require (
 	github.com/Shopify/gomail v0.0.0-20220729171026-0784ece65e69
+	github.com/go-pdf/fpdf v0.9.0
 	github.com/joho/godotenv v1.5.1
 )
 
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 
 	"github.com/Shopify/gomail"
+	"github.com/go-pdf/fpdf"
 	"github.com/joho/godotenv"
 )
 
@@ -18,6 +19,14 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file: ", err)
 	}
+
+	// Create PDF
+	log.Println("Creating PDF")
+	pdf := fpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 16)
+	pdf.Cell(40, 10, "Hello World!")
+	pdf.OutputFileAndClose("./test.pdf")
 
 	smtpPort, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
 	if err != nil {
@@ -38,12 +47,20 @@ func main() {
 	m.SetHeader("To", fmt.Sprintf("Test <%s>", os.Getenv("SMTP_USER")))
 	m.SetHeader("Subject", "Hello!")
 	m.SetBody("text/html", "Hello <b>World</b>!")
-	m.Attach("./test.txt")
+	m.Attach("./test.pdf")
 
 	err = d.DialAndSend(m)
 	if err != nil {
 		log.Fatal("Error sending email: ", err)
 	} else {
 		log.Println("Email sent!")
+	}
+
+	log.Println("Deleting PDF")
+	err = os.Remove("./test.pdf")
+	if err != nil {
+		log.Fatal("Error deleting PDF: ", err)
+	} else {
+		log.Println("PDF deleted!")
 	}
 }
